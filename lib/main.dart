faa766b5baf97dffa6fb551ebae431a5d4e66b38
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-03-13 01:13:43 -0300

    First commit - app creation and introduction to Stateful Widgets

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello You',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HelloYou(),
+    );
+  }
+}
+
+class HelloYou extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HelloYouState();
+}
+
+class _HelloYouState extends State<HelloYou> {
+  String name = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (String typedName) {
+                setState(() {
+                  name = typedName;
+                });
+              },
+            ),
+            Text("Hello " + name + (name.isNotEmpty ? "!" : ""))
+          ],
+        ),
+      ),
+    );
+  }
+}
